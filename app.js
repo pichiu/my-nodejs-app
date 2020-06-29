bf2b725391218ce4863fa3b4b4ecb3aa884f7b34
pi_chiu <Pi_Chiu@asus.com>
2020-06-29 15:21:44 +0800

    for webhook test push image

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ module.exports = (/*options*/) => {
   app.get('/', (req, res) => {
     // Use req.log (a `pino` instance) to log JSON:
     req.log.info({message: 'Hello!'});
-    res.send('Hello from webhook rename!');
+    res.send('Hello from webhook fix docker!');
   });
 
   return app;
